5087b8c4a2b198990b2a15e938ad3aa88b7fcb69
ferron gerald <opstyle1@hotmail.fr>
2021-07-07 09:02:54 +0100

    mise en place eval et condition

--- index.js
+++ index.js
@@ -6,29 +6,32 @@ function addition(x) {
 function zero(x) {
   result = 0;
 }
-addition(3);
-addition(7);
-addition(7);
-console.log(result);
 
 let boutons = document.querySelectorAll(".button");
 let resultat = document.querySelector(".resultat");
+// let zero = document.getElementById("zero");
 
 boutons.forEach((bouton) => {
   bouton.addEventListener("click", (e) => {
+    // if (e.target.id == "zero" && resultat.textContent == "") {
+    //   resultat.textContent = "";
+    //   console.log("salut");
+    // } else
     if (
       (result === 0 || result < 0 || result > 0) &&
-      e.target.innerText !== "CE"
+      e.target.innerText !== "CE" &&
+      e.target.innerText !== "="
     ) {
       resultat.textContent += e.target.innerText;
     } else if (e.target.innerText == "CE") {
-      resultat.textContent = 0;
+      resultat.textContent = "";
+    } else if (e.target.innerText === "=") {
+      resultat.textContent = eval(resultat.textContent);
     } else {
-      result = 0;
+      result = "";
       resultat.textContent = result;
     }
     console.log(e.target.innerText);
+    console.log(resultat.textContent);
   });
 });
-
-console.log(result);
